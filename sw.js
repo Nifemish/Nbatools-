53385e47ecd65c5bfd0f94799ef24d9e9b7809c9
Nifemish <149886675+Nifemish@users.noreply.github.com>
2026-09-05 10:52:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Navigations use the network first so a newly uploaded HTML file is picked
  * up without asking users to clear browser or app data.
  */
-const TRACKPORT_CACHE = 'trackport-runtime-v6';
+const TRACKPORT_CACHE = 'trackport-runtime-v7';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
